20a519801b686c8592e71e65083316ad7c0bf170
Hubol <hubol.gordon@gmail.com>
2020-04-27 17:36:00 -0500

    Add some music

--- src/musics.ts
+++ src/musics.ts
@@ -1,3 +1,18 @@
 import { Howl } from 'howler';
     
 // This file is generated. Do not touch.
+
+export const Intro = new Howl({
+    src: [require("./music/intro.ogg"), require("./music/intro.mp3")],
+    preload: false
+});
+
+export const Overworld = new Howl({
+    src: [require("./music/overworld.ogg"), require("./music/overworld.mp3")],
+    preload: false
+});
+
+export const WoodedArea1 = new Howl({
+    src: [require("./music/wooded area1.ogg"), require("./music/wooded area1.mp3")],
+    preload: false
+});
