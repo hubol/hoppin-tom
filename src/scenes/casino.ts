e7fe2cb9810aa9fd79a1a90c3b4b61be282ee091
Hubol <hubol.gordon@gmail.com>
2020-04-29 12:00:06 -0500

    Add exit sign to casino

--- src/scenes/casino.ts
+++ src/scenes/casino.ts
@@ -1,10 +1,11 @@
 import {playMusicAsync} from "../playMusic";
 import {WoodedArea1} from "../musics";
 import {game} from "../tom/game";
-import {CasinoBackground, SmallTom, SpinButton} from "../textures";
+import {CasinoBackground, CasinoExitSign, SmallTom, SpinButton} from "../textures";
 import { Sprite } from "pixi.js";
 import {Key} from "../utils/key";
 import {DropShadowFilter} from "@pixi/filter-drop-shadow";
+import {worldMap} from "./worldMap";
 
 export async function casino()
 {
@@ -14,12 +15,12 @@ export async function casino()
     background.height = game.height;
 
     const spinButton = Sprite.from(SpinButton);
-    spinButton.position.set(game.width - 12, 12);
+    spinButton.position.set(game.width - 12, 64);
     spinButton.anchor.set(0.5, 0.5);
 
     const smallTom = Sprite.from(SmallTom);
     smallTom.anchor.set(0.5, 0.5);
-    smallTom.position.set(12, 12);
+    smallTom.position.set(32, 12);
     smallTom.withStep(() => {
         const xprev = smallTom.x;
         const yprev = smallTom.y;
@@ -33,6 +34,8 @@ export async function casino()
         else if (Key.isDown("ArrowDown"))
             smallTom.y += 1;
 
+        smallTom.x = Math.min(game.width, Math.max(0, smallTom.x));
+        smallTom.y = Math.min(game.height, Math.max(0, smallTom.y));
         smallTom.angle += smallTom.x - xprev + smallTom.y - yprev;
     });
 
@@ -40,8 +43,15 @@ export async function casino()
         spinButton.tint = spinButton.collides(smallTom) ? 0x999999 : 0xffffff;
     });
 
+    const casinoExitSign = Sprite.from(CasinoExitSign);
+    casinoExitSign.position.set(4, 4);
+    casinoExitSign.withStep(() => {
+        if (smallTom.collides(casinoExitSign))
+            game.goto(worldMap);
+    });
+
     const dropShadowFilter = new DropShadowFilter({distance: 3, alpha: 0.5, quality: 3, blur: 1});
     smallTom.filters = [dropShadowFilter];
 
-    game.stage.addChild(background, spinButton, smallTom);
+    game.stage.addChild(background, spinButton, casinoExitSign, smallTom);
 }
